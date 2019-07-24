45923ce5d2d40d4f7382fde7927f5701b0c1bd3a
Tempest McCabe <tmccabe@bu.edu>
2019-07-24 16:24:00 -0400

    Making .sh file 1-27

--- humidity_binomial.sh
+++ humidity_binomial.sh
@@ -2,7 +2,7 @@
 #$ -l h_rt=72:00:00
 #$ -l buyin
 #$ -N humidity_binomial
-#$ -t 1-5
+#$ -t 1-27
 #$ -o /usr3/graduate/tmccabe/Maine_Tick_Disease_Forecast/qsub_output
 module load R
 Rscript /usr3/graduate/tmccabe/Maine_Tick_Disease_Forecast/geo_BinomProbYearHumidity.R
